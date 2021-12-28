0bfa12d7cc5a179763d60284ce3ee3146b091e5a
DEEP-2009 <96776043+DEEP-2009@users.noreply.github.com>
2021-12-28 17:15:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_qality:90
+});
+
+image = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+});
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hXUNEHlam/model.json' , modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
